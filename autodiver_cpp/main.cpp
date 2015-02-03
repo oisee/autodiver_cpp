3734d5907ddfc516a7840d49d48c4782b414d6c4
oisee <ooisee@gmail.com>
2015-02-03 17:16:14 +0500

    iterate through folder

--- autodiver_cpp/main.cpp
+++ autodiver_cpp/main.cpp
@@ -8,22 +8,46 @@
 
 #define CELLX 8
 #define CELLY 8
+#define DIR_EVAL "./eval"
 
 #include <iostream>
 #include <string>
 #include <Magick++.h>
+#include <dirent.h>
 
 using namespace std;
 using namespace Magick;
 
+typedef pair<int,Color> ColorRating;
+typedef tuple<int, string, string> ResultRow;
+
 int eval_image( string image_path );
 int eval_cell( Image img, int x, int y);
+bool rating_reverse_compare( ColorRating first, ColorRating second );
 int main(int argc, const char * argv[]) {
     // insert code here...
-
+    
     int err = 0;
     
-    err = eval_image("/Users/oisee/dev/imt/eval/geometry.png_r0_s256_x2_y4.png");
+//  err = eval_image("/Users/oisee/dev/imt/eval/geometry.png_r0_s256_x2_y4.png");
+//  err = eval_image("/Users/oisee/dev/imt/in.jpg");
+    err = eval_image("/Users/oisee/dev/imt/peep.png");
+    cout << "File Err: " << err << endl;
+    
+    
+    DIR *dp;
+    struct dirent *entry;
+    dp = opendir(DIR_EVAL);
+    if ( dp == NULL ) {
+        perror("opendir: Path './eval' does not exist or could not be read.");
+        return -1;
+    }
+    
+    while ((entry = readdir(dp))){
+        puts(entry->d_name);
+        
+    }
+    closedir(dp);
     
     return 0;
 }
@@ -54,21 +78,34 @@ int eval_cell( Image img, int x, int y){
     
     typedef map<Color, int> ColorStatistic;
     ColorStatistic cs;
-    for (int i; i< CELLX * CELLY; i++) {
-        Color pixel = pp[i];
-        cs[pixel]++;
+    for (int i = 0 ; i< CELLX * CELLY; i++) {
+        cs[pp[i]]++;
     }
     
-    cout << "Cell size:" << cs.size() << endl;
-    
-    if (cs.size() < 0 ) {
+    if (cs.size() <= 2 ) {
         return 0;
     } else {
-        //-----------------------
+        
+        vector<ColorRating> ss;
+        
         for (auto &i : cs) {
-            cout << "Cell: " << i.second << "" << endl;
+            ss.push_back(make_pair(i.second, i.first));
         }
         
+        sort(ss.begin(),ss.end(), rating_reverse_compare );
+        
+        int counter = 0;
+        for (auto &p : ss) {
+            counter ++;
+            if (counter > 2) {
+                err += p.first;
+            }
+        }
+    
         return err;
     }
+}
+
+bool rating_reverse_compare( ColorRating first, ColorRating second ){
+    return first.first > second.first;
 }
